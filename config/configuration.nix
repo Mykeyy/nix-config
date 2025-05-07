3bc883aa7cd624678f91d863bed9ed4ddaddff49
InvraNet <pacstrap@mx.idalon.org>
2025-05-08 06:13:30 +1000

    Add: correct details.

--- config/configuration.nix
+++ config/configuration.nix
@@ -32,18 +32,27 @@ user: system: desktop:
   };
 
   hardware.nvidia = {
-    # https://nixos.wiki/wiki/Nvidia
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    # prime = {
-    #  sync.enable = true;
-    #  nvidiaBusId = "PCI:1:0.0"; 
-    #  intelBusId = "PCI:0:2.0";
-    # };
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "575.51.02";
+      sha256_64bit = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
+      sha256_aarch64 = "sha256-NNeQU9sPfH1sq3d5RUq1MWT6+7mTo1SpVfzabYSVMVI=";
+      openSha256 = "sha256-NQg+QDm9Gt+5bapbUO96UFsPnz1hG1dtEwT/g/vKHkw=";
+      settingsSha256 = "sha256-6n9mVkEL39wJj5FB1HBml7TTJhNAhS/j5hqpNGFQE4w=";
+      persistencedSha256 = "sha256-dgmco+clEIY8bedxHC4wp+fH5JavTzyI1BI8BxoeJJI=";
+    };
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
   };
 
 
